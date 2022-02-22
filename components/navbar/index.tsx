7ad734b9c4f1b2f6347b017cf2d1f6df3b7071bf
Michael Hayes <hayes.michael89@gmail.com>
2022-02-22 08:21:09 +0200

    adding more of the tools

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,14 +1,131 @@
-import React from 'react';
-import Link from 'next/link'
-import Image from 'next/image'
-import { AppBar } from '@mui/material';
-import styles from './navbar.module.scss'
-import rzLogo from '../../assets/images/RZLogo.png';
-import Button from '@mui/material/Button';
-const Navbar : React.FC = ()=>{
-  return( 
-    <>
-    {/* <div className={styles.navbarWrapper}>
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./navbar.module.scss";
+import rzLogo from "../../assets/images/RZLogo.png";
+
+/*
+    MUIv5 Appbar
+*/
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Home", "About", "Roadmap", "Pricing", "Contact Us"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+/*
+    Tabs
+*/
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const Navbar: React.FC = () => {
+  /*
+    Appbar tings
+  */
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  /*
+  Tabs tings
+*/
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box>
+            <Typography>Resource Zen</Typography>
+          </Box>
+          <Box sx={{ width: "100%" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="secondary"
+              indicatorColor="secondary"
+              aria-label="secondary tabs example"
+            >
+              <Tab value="one" label="Item One" />
+              <Tab value="two" label="Item Two" />
+              <Tab value="three" label="Item Three" />
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
+
+/* <div className={styles.navbarWrapper}>
     <h1>Resource Zen</h1>
     <ul>
       <li><Link href="/">Home</Link></li>
@@ -32,11 +149,5 @@ const Navbar : React.FC = ()=>{
         height={100}
       />
     </div>
-  </div> */}
-  <AppBar></AppBar>
-    </>
-  
-  );
-}
-
-export default Navbar;
+  </div> 
+  */
