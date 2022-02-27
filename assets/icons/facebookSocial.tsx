5bb89af0bc3f5a3f9cb5fcb4a814673424341338
Michael Hayes <hayes.michael89@gmail.com>
2022-02-28 01:47:02 +0200

    landing page v1

--- assets/icons/facebookSocial.tsx
+++ assets/icons/facebookSocial.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-const FacebookSocial =()=> {
+type Props = {
+  sx?: {};
+};
+
+const FacebookSocial = (props: Props) => {
   return (
     <>
-        <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="23" cy="23" r="21.5" fill="white" stroke="#49B295" strokeWidth="3"/>
-        <path d="M24.6667 19.6667V17.6667C24.6667 16.8 24.8667 16.3333 26.2667 16.3333H28V13H25.3333C22 13 20.6667 15.2 20.6667 17.6667V19.6667H18V23H20.6667V33H24.6667V23H27.6L28 19.6667H24.6667Z" fill="#49B295"/>
-        </svg>
+      <svg
+        width="46"
+        height="46"
+        viewBox="0 0 46 46"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={props.sx}
+      >
+        <circle
+          cx="23"
+          cy="23"
+          r="21.5"
+          fill="white"
+          stroke="#49B295"
+          strokeWidth="3"
+        />
+        <path
+          d="M24.6667 19.6667V17.6667C24.6667 16.8 24.8667 16.3333 26.2667 16.3333H28V13H25.3333C22 13 20.6667 15.2 20.6667 17.6667V19.6667H18V23H20.6667V33H24.6667V23H27.6L28 19.6667H24.6667Z"
+          fill="#49B295"
+        />
+      </svg>
     </>
-  )
-}
+  );
+};
 
-export default FacebookSocial
+export default FacebookSocial;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,196 @@
-import React from 'react'
-import Head from 'next/head'
-//Socials
-import FacebookSocial from 'assets/icons/facebookSocial'
-import LinkedInSocial from 'assets/icons/linkedinSocial'
-import YoutubeSocial from 'assets/icons/youtubeSocial'
-import InstagramSocial from 'assets/icons/instagramSocial'
+import React from 'react';
+import Head from 'next/head';
+// Socials
+import FacebookSocial from 'assets/icons/facebookSocial';
+import LinkedInSocial from 'assets/icons/linkedinSocial';
+import YoutubeSocial from 'assets/icons/youtubeSocial';
+import InstagramSocial from 'assets/icons/instagramSocial';
 
-//Cards
-import JobcardIcon from 'assets/icons/jobcardIcon'
-import InvoiceIcon from 'assets/icons/invoiceIcon'
-import StockIcon from 'assets/icons/stockIcon'
+// Cards
+import JobcardIcon from 'assets/icons/jobcardIcon';
+import InvoiceIcon from 'assets/icons/invoiceIcon';
+import StockIcon from 'assets/icons/stockIcon';
 
-const Home: React.FC = ()=> {
+// Background
+import BlobBackground from '../assets/backgrounds/blobBackground';
+import LandingArt from '../assets/art/landingArt';
+import {
+  Container,
+  Box,
+  Typography,
+  Button,
+  Grid,
+  Paper,
+  IconButton,
+} from '@mui/material';
+
+type Props = {
+  sx?: {};
+};
+type Service = {
+  icon: (props: Props) => JSX.Element;
+  title: string;
+  extra?: string;
+};
+const services: Service[] = [
+  {
+    icon: JobcardIcon,
+    title: 'JOBCARDS',
+  },
+  {
+    icon: InvoiceIcon,
+    title: 'INVOICE',
+    extra: 'INTEGRATION',
+  },
+  {
+    icon: StockIcon,
+    title: 'JOBCARDS',
+  },
+];
+
+const Home: React.FC = () => {
   return (
-    <div className="homePage">
+    <div
+      className="div"
+      style={{
+        position: 'relative',
+        overflow: 'visible',
+        width: '100%',
+      }}
+    >
+      <BlobBackground
+        sx={{
+          position: 'absolute',
+          zIndex: '-20',
+          width: '200%',
+          left: '-50%',
+        }}
+      ></BlobBackground>
+      <LandingArt
+        sx={{
+          position: 'absolute',
+          zIndex: '-10',
+          right: '0%',
+          top: '15%',
+        }}
+      ></LandingArt>
+      <Container
+        maxWidth="xl"
+        sx={{ position: 'relative', overflow: 'visible' }}
+      >
+        <Head>
+          <title key="title">Resource Zen</title>
+        </Head>
+
+        <Box sx={{ marginBottom: '40vh', marginTop: '5vh', width: '48%' }}>
+          <Typography variant="h2">
+            Let Resource Zen make life easier for you.
+          </Typography>
+          <Typography variant="subtitle1">
+            We help you customise your business resources such as, job cards,
+            invoices, stock list, and much more.{' '}
+          </Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{
+              paddingLeft: '5%',
+              paddingRight: '5%',
+              marginTop: '3%',
+              marginBottom: '3%',
+            }}
+          >
+            <Typography variant="h3">GET STARTED</Typography>
+          </Button>
+          <Box>
+            <IconButton>
+              <FacebookSocial />
+            </IconButton>
+            <IconButton>
+              <LinkedInSocial />
+            </IconButton>
+            <IconButton>
+              <YoutubeSocial />
+            </IconButton>
+            <IconButton>
+              <InstagramSocial />
+            </IconButton>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            marginTop: '60vh',
+            display: 'grid',
+            placeItems: 'center',
+            width: '100%',
+            border: 'none',
+          }}
+        >
+          <Typography
+            variant="h2"
+            textAlign="center"
+            sx={{ fontFamily: 'Poppins', color: 'white' }}
+          >
+            OUR FEATURES
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            textAlign="center"
+            sx={{ fontFamily: 'Poppins', color: 'white', fontWeight: '400' }}
+          >
+            Velit officia consequat duis enim velit mollit. Exercitation veniam
+            consequat sunt nostrud amet.
+          </Typography>
+
+          <Grid
+            container
+            spacing={1}
+            sx={{
+              marginTop: '5vh',
+              border: 'none',
+            }}
+          >
+            {services.map((service, index) => {
+              return (
+                <Grid item xs={4} key={service.title} sx={{ border: 'none' }}>
+                  <Paper
+                    sx={{
+                      display: 'grid',
+                      placeItems: 'center',
+                      height: '45vh',
+                      width: '90%',
+                    }}
+                  >
+                    {index === 0 ? (
+                      <JobcardIcon />
+                    ) : index === 1 ? (
+                      <InvoiceIcon />
+                    ) : (
+                      <StockIcon />
+                    )}
+                    <Typography variant="h4" sx={{ fontFamily: 'Poppins' }}>
+                      {service.title}
+                    </Typography>
+                    {service.extra && (
+                      <Typography variant="h4" sx={{ fontFamily: 'Poppins' }}>
+                        {service.extra}
+                      </Typography>
+                    )}
+                  </Paper>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
+
+{
+  /* <div className="homePage">
         <Head>
           <title key="title">Resource Zen</title>
         </Head>
@@ -45,8 +222,5 @@ const Home: React.FC = ()=> {
                 </div>
             </div>
         </div>
-    </div>
-  )
+    </div> */
 }
-  
-export default Home
